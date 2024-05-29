7cc4df13135ab2eb4f85cd6eef2c3d0399a3dbd8
AniketShende <aniketshende1432@gmail.com>
2024-05-29 16:00:27 +0530

    home and sign page

---
+++ frontend/src/components/about/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./about.css";
+const About = () => {
+  return (
+    <div className='about d-flex justify-content-center align-items-center'>
+        <div className="container ">
+        <div className="d-flex">
+            <h1>About Us</h1>
+        </div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae pariatur repudiandae ipsum tempore. Magni porro fugiat nemo 
+            aliquam? Saepe aut aliquid atque commodi optio nam ipsa modi, nobis tenetur odit?Lorem ipsum dolor sit amet consectetur 
+            adipisicing elit. Animi exercitationem nostrum ea architecto similique ratione fugiat ducimus voluptate distinctio, eum 
+            quos repudiandae dolor aliquid perspiciatis consequatur aliquam autem soluta vitae!Lorem Lorem ipsum dolor sit, amet 
+            consectetur adipisicing elit. Et qui mollitia totam ea alias aperiam molestiae autem perspiciatis. Reiciendis, adipisci 
+            nobis mollitia eaque delectus voluptate obcaecati a fugiat provident sapiente.</p>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ frontend/src/components/signup/Signin.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Signup.css";
+import HeadingComp from './HeadingComp';
+const Signin = () => {
+  return (
+    <div><div className='signup'>
+    <div className="container">
+        <div className="row">
+        <div className="col-lg-5 column  d-flex justify justify-content-center align-items-center">
+                <HeadingComp first="Sign" second="In"/>
+            </div>
+            <div className="col-lg-7 column d-flex justify justify-content-center align-items-center">
+                <div className='d-flex flex-column w-100 p-5'>
+                    <input className='p-2 my-3 input-signup' type="email" name="email" placeholder='Enter Your Email' />
+                    <input className='p-2 my-3 input-signup' type="password" name="password" placeholder='Enter Your Passwprd' />
+                    <button className='btn-signup p-2'>SignIn</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div></div>
+  )
+}
+
+export default Signin
